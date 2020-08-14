bf294725b13b7213d636128f4d194849a834eddf
chuenwei0129 <chuenwei0129@gmail.com>
2020-08-14 23:12:33 +0800

    update

--- examples/生成器异步.js
+++ examples/生成器异步.js
@@ -1,2 +1,39 @@
 const fs = require('fs')
-const path = require('path')
+
+// fs.readFile('./examples/generator1.html', (err, data) => {
+// 	if (err) throw err
+// 	console.log(data.toString())
+// })
+
+// promise
+let readHtml = new Promise((resolve, reject) => {
+	fs.readFile('./examples/generator1.html', (err, data) => {
+		if (err) reject(err)
+		resolve(data)
+	})
+})
+
+let res = readHtml.then(
+	(val) => {
+		console.log(val.toString())
+		return '123'
+	},
+	(err) => console.log(err)
+)
+res.then((val) => console.log(val))
+
+// function readHtml() {
+// 	fs.readFile('./examples/generator1.html', (err, data) => {
+// 		if (err) throw err
+// 		iterator.next(data)
+// 	})
+// }
+
+// function* gen() {
+// 	let data = yield readHtml()
+// 	// 处理数据
+// 	console.log(data.toString())
+// }
+
+// let iterator = gen()
+// iterator.next()
